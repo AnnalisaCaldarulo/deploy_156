bea9359759fa640b303da454106d955add1ca20f
Annalisa Caldarulo <annalisacaldarulo97@gmail.com>
2024-12-03 14:59:30 +0100

    messaggi d'errore

--- resources/views/livewire/create-form.blade.php
+++ resources/views/livewire/create-form.blade.php
@@ -1,9 +1,20 @@
 <form class="p-5 rounded shadow bg-sec" wire:submit="saveArticle">
-    @if (session('success'))
-        <div class="alert alert-success text-center">
-            {{ session('success') }}
-        </div>
-    @endif
+    <div class="d-flex justify-content-center">
+        @if (session('success'))
+            <div class="alert alert-success text-center w-75">
+                {{ session('success') }}
+            </div>
+        @endif
+        @if ($errors->any())
+            <div class="alert alert-danger w-50">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </div>
     <div class="mb-3">
         <label for="title" class="form-label">Titolo</label>
         <input type="text" id="title" class="form-control border-success" wire:model="title">
