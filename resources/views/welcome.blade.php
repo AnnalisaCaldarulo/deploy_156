0b61fce36571aabc2e01ffa60b28c4996a4863d4
Annalisa Caldarulo <annalisacaldarulo97@gmail.com>
2024-12-03 10:29:50 +0100

    scaffolding

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <h1 class="display-3 text-center pt-5 text-white">
+                    Deploy 156
+                </h1>
+            </div>
+        </div>
+    </div>
+</x-layout>
